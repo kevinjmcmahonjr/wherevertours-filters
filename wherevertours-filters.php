49402b733c623f8e263b824232a0f73d1be9cd24
kevinjmcmahonjr <kevinjmcmahonjr@gmail.com>
2018-05-04 13:32:31 -0400

    Initial Commit

---
+++ wherevertours-filters.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: Wherever Tours Filters
+Plugin URI:
+Description: General Filters and Actions to adjust names and phrases through-out WordPress
+Version: 1.0
+Author: Kevin J. McMahon Jr.
+Author URI:
+License:GPLv2
+*/
+?>
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+// Hide Admin Bar
+if (!current_user_can('edit_posts')) {
+	add_filter('show_admin_bar', '__return_false');
+}
+
+// Rename Registration
+function tml_action_template_message_filter( $message, $action ) {
+    if ( 'register' == $action )
+        return 'Create Account';
+    return $message;
+}
+add_filter( 'tml_action_template_message', 'tml_action_template_message_filter', 10, 2 );
+?>
