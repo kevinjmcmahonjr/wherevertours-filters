1ae859e81f7e2842b823c4e55faff55475bd49be
kevinjmcmahonjr <kevinjmcmahonjr@gmail.com>
2018-05-11 12:08:00 -0400

    Syntax Update

--- wherevertours-filters.php
+++ wherevertours-filters.php
@@ -15,7 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Hide Admin Bar
-function hide_travelers_admin_bar($content){
+function hide_travelers_admin_bar(){
 	if (!current_user_can('edit_posts'){
 		show_admin_bar(false);
 	}
